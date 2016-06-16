dbef4bc43f55e4d911a24b4d9b0e2b35cb998e5c
Aayush Dawra <aayushgopaldawra@gmail.com>
2016-06-16 13:39:48 -0400

    Resolving merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604185812) do
+ActiveRecord::Schema.define(version: 20160615172131) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id",      limit: 4
@@ -91,8 +91,10 @@ ActiveRecord::Schema.define(version: 20160604185812) do
     t.float    "voting",            limit: 24
     t.integer  "rate_num",          limit: 4
     t.integer  "vote_num",          limit: 4
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.boolean  "is_approved",                   default: false, null: false
+    t.string   "video_desc",        limit: 255
   end
 
 end
