27f32a220a1190543504894e25558326b1c48d61
Ziping Zheng <nkuzzp@gmail.com>
2016-06-25 22:00:54 -0400

    db:seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,18 @@
   title  = "sample#{n+1}"
   video_url = "sample#{n+1}.mp4"
   poster_url = "sample#{n+1}.jpg"
-  Video.create!(title: title, video_url: video_url, poster_url: poster_url, uploader_id: "#{n%3+1}", tags: "yes;no;notsure")
+  tags = ["funny:pet", "pet:animal", "animal;funny;pet"]
+  Video.create!(title: title, video_url: video_url, poster_url: poster_url, uploader_id: "#{n%3+1}", tags: tags[n%3])
 end
 
 10.times do |n|
   email  = "a#{n+1}@c3.com"
   password = "0000"
   User.create!(email: email, password: password)
+end
+
+10.times do |n|
+  vid  = n+1
+  cid = n%3+1
+  VcRelation.create!(video_id: vid, competition_id: cid)
 end
