bdfad3c2d7c52c728f183d8df727f1a6b3602151
Ziping Zheng <nkuzzp@gmail.com>
2016-07-03 02:31:16 -0400

    July 2nd version, with competition management more refined

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module CucucityApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "images")
 
     config.generators do |g|
       g.test_framework :rspec,
